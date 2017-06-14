9c38d6a2fba5ac50a3fa4a9ec69db7bc641d38fa
Davy'd <pdaswift3009@gmail.com>
2017-06-14 13:07:26 +0100

    add local user variable

--- app.js
+++ app.js
@@ -76,7 +76,10 @@ app.use(function(req, res, next) {
     next();
 });
 
-
+app.get('*', function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+})
 app.use('/', index);
 app.use('/users', users);
 
