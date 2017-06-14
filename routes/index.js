e3809f5bac45d9b43aa5bd19a861cf9550e33aa4
Davy'd <pdaswift3009@gmail.com>
2017-06-14 12:29:23 +0100

    ensure auth for members area

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthentication, function(req, res, next) {
     res.render('index', { title: 'Members' });
 });
 
+
+
+function ensureAuthentication(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/users/login');
+}
 module.exports = router;
